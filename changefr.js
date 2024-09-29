bb1b456b618a171a470e2641637489272740cbd2
Tonia2006 <kv.antonina.g@gmail.com>
2024-09-29 15:42:27 +0300

    test

--- changefr.js
+++ changefr.js
@@ -45,27 +45,27 @@ function changeBlockContentBraclet() {
     blockElement.innerHTML = `
         <a href="goodukr.html" class="good">
             <img src="photo_2023-01-31_21-31-38.jpg">
-            <p>Ціна: 100грн</p>
+            <p>Prix:   10euro</p>
         </a>
         <a href="good1ukr.html" class="good">
             <img src="photo_2024-06-15_21-19-00.jpg">
-            <p>Ціна: 100грн</p>
+            <p>Prix:   10euro</p>
         </a>
         <a href="" class="good">
             <img src="photo_2024-06-15_21-19-06.jpg">
-            <p>Ціна: 100грн</p>
+            <p>Prix:   10euro</p>
         </a>
         <a href="" class="good">
             <img src="photo_2024-06-15_21-19-09.jpg">
-            <p>Ціна: 100грн</p>
+            <p>Prix:   10euro</p>
         </a>
         <a href="" class="good">
             <img src="photo_2024-06-15_21-19-12.jpg">
-            <p>Ціна: 100грн</p>
+            <p>Prix:   10euro</p>
         </a>
         <a href="" class="good">
             <img src="photo_2024-06-15_21-26-56.jpg">
-            <p>Ціна: 100грн</p>
+            <p>Prix:   10euro</p>
         </a>
     `;
 }
@@ -75,7 +75,7 @@ function changeBlockContentNecklace() {
     blockElement.innerHTML = `
         <a href="" class="good">
             <img src="pexels-noellegracephotos-906056.jpg">
-            <p>Ціна: 100грн</p>
+            <p>Prix:   10euro</p>
         </a>
     `;
 }
@@ -85,7 +85,7 @@ function changeBlockContentEarrings() {
     blockElement.innerHTML = `
         <a href="" class="good">
             <img src="pexels-lara-artemova-784501-1648741.jpg">
-            <p>Ціна: 100грн</p>
+            <p>Prix:   10euro</p>
         </a>
     `;
 }
@@ -95,7 +95,7 @@ function changeBlockContentOther() {
     blockElement.innerHTML = `
         <a href="" class="good">
             <img src="pexels-alena-yanovich-37077498-14180685.jpg">
-            <p>Ціна: 100грн</p>
+            <p>Prix:   10euro</p>
         </a>
     `;
 }
