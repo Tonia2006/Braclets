0b6325cbd6ad1423afe4ac7825434c4e7a842464
Tonia2006 <kv.antonina.g@gmail.com>
2024-09-29 14:26:56 +0300

    changejs

---
+++ change.js
@@ -0,0 +1,97 @@
+function changeBlockContent(){
+    let blockElement = document.getElementById("c");
+    blockElement.innerHTML = `
+    <a href="goodukr.html" class="good">
+        <img src="photo_2023-01-31_21-31-38.jpg">
+        <p>Ціна: 100грн</p>
+    </a>
+    <a href="good1ukr.html" class="good">
+        <img src="photo_2024-06-15_21-19-00.jpg">
+        <p>Ціна: 100грн</p>
+    </a>
+    <a href="" class="good">
+        <img src="photo_2024-06-15_21-19-06.jpg">
+        <p>Ціна: 100грн</p>
+    </a>
+    <a href="" class="good">
+        <img src="photo_2024-06-15_21-19-09.jpg">
+        <p>Ціна: 100грн</p>
+    </a>
+    <a href="" class="good">
+        <img src="photo_2024-06-15_21-19-12.jpg">
+        <p>Ціна: 100грн</p>
+    </a>
+    <a href="" class="good">
+        <img src="photo_2024-06-15_21-26-56.jpg">
+        <p>Ціна: 100грн</p>
+    </a>
+    <a href="" class="good">
+        <img src="pexels-alena-yanovich-37077498-14180685.jpg">
+        <p>Ціна: 100грн</p>
+    </a>
+    <a href="" class="good">
+        <img src="pexels-lara-artemova-784501-1648741.jpg">
+        <p>Ціна: 100грн</p>
+    </a>
+    <a href="" class="good">
+        <img src="pexels-noellegracephotos-906056.jpg">
+        <p>Ціна: 100грн</p>
+    </a>    `;
+}
+
+function changeBlockContentBraclet(){
+    let blockElement = document.getElementById("c");
+    blockElement.innerHTML = `
+    <a href="goodukr.html" class="good">
+        <img src="photo_2023-01-31_21-31-38.jpg">
+        <p>Ціна: 100грн</p>
+    </a>
+    <a href="good1ukr.html" class="good">
+        <img src="photo_2024-06-15_21-19-00.jpg">
+        <p>Ціна: 100грн</p>
+    </a>
+    <a href="" class="good">
+        <img src="photo_2024-06-15_21-19-06.jpg">
+        <p>Ціна: 100грн</p>
+    </a>
+    <a href="" class="good">
+        <img src="photo_2024-06-15_21-19-09.jpg">
+        <p>Ціна: 100грн</p>
+    </a>
+    <a href="" class="good">
+        <img src="photo_2024-06-15_21-19-12.jpg">
+        <p>Ціна: 100грн</p>
+    </a>
+    <a href="" class="good">
+        <img src="photo_2024-06-15_21-26-56.jpg">
+        <p>Ціна: 100грн</p>
+    `;
+}
+
+function changeBlockContentNecklace(){
+    let blockElement = document.getElementById("c");
+    blockElement.innerHTML = `
+    <a href="" class="good">
+        <img src="pexels-noellegracephotos-906056.jpg">
+        <p>Ціна: 100грн</p>
+    </a>    `;
+}
+
+function changeBlockContentEarrings(){
+    let blockElement = document.getElementById("c");
+    blockElement.innerHTML = `
+    <a href="" class="good">
+        <img src="pexels-lara-artemova-784501-1648741.jpg">
+        <p>Ціна: 100грн</p>
+    </a> `;
+}
+
+function changeBlockContentOther(){
+    let blockElement = document.getElementById("c");
+    blockElement.innerHTML = `
+    <a href="" class="good">
+        <img src="pexels-alena-yanovich-37077498-14180685.jpg">
+        <p>Ціна: 100грн</p>
+    </a> `;
+}
+
